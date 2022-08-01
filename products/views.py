6576330e8b159bc8fc6860923e440c9a96e3bb64
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-08-01 12:46:50 +0000

    add sorting and direction view

--- products/views.py
+++ products/views.py
@@ -11,9 +11,25 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     category = None
-
+    sort = None
+    direction = None
     
     if request.GET:
+        """view to sort the prducts"""
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+
         """ view to check the category """
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
@@ -29,14 +45,14 @@ def all_products(request):
             
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
-
-    """ view to check the category """
-        
+    
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
         'search_term': query,
         'current_category': category,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
