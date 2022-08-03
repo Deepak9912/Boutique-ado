3caa022312ddc3b7f6c95588fcd056dc01255977
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-08-03 19:01:01 +0000

    fixed url for product detail

--- bag/views.py
+++ bag/views.py
@@ -7,8 +7,9 @@ def view_bag(request):
 
     return render(request, 'bag/bag.html')
 
+
 def add_to_bag(request, item_id):
-    """Add a quantity of the specified product to the shopping bag"""
+    """ Add a quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -18,7 +19,6 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
-    
+
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
+
 from .models import Product, Category
 
 # Create your views here.
@@ -54,7 +55,6 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
-
 
 
 def product_detail(request, product_id):
